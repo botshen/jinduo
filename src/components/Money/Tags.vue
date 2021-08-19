1e369a60f576f9ca4afe17acd609b6f4b21c4f49
Xin shen <363088847@qq.com>
2021-08-19 12:24:54 +0800

    Tags.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -19,15 +19,17 @@ import {Component, Prop} from 'vue-property-decorator';
 
 @Component({
   computed: {
-    tagList(){
-      // TODO
-      // return this.$store.fetchTags();
-      return []
+    tagList() {
+      return this.$store.state.tagList;
     }
   }
 })
 export default class Tags extends Vue {
   selectedTags: string[] = [];
+
+  created() {
+    this.$store.commit('fetchTags');
+  }
 
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
@@ -42,8 +44,7 @@ export default class Tags extends Vue {
   create() {
     const name = window.prompt('请输入标签名');
     if (!name) { return window.alert('标签名不能为空'); }
-    // TODO
-    // store.createTag(name);
+    this.$store.commit('createTag', name);
   }
 }
 </script>
@@ -60,7 +61,7 @@ export default class Tags extends Vue {
     display: flex;
     flex-wrap: wrap;
     > li {
-      $bg: #d9d9d9;
+      $bg: #D9D9D9;
       background: $bg;
       $h: 24px;
       height: $h;
